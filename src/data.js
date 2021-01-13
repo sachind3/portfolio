599b406ba51fe15a2d362c21df4099487bb28a1b
Sachin Desai <desai.sachin45@gmail.com>
2021-01-13 18:24:43 +0530

    ui changes

--- src/data.js
+++ src/data.js
@@ -1,204 +1,226 @@
 export const userInfo = {
-    name: 'Sachin Desai',
-    image: 'https://picsum.photos/300/300',
-    designation: 'Front-end developer',
-    detail: ["Self-motivated developer, who is willing to learn and create outstanding UI applications. Donec aliquam est dui, vel vestibulum diam sollicitudin id.Quisque feugiat malesuada molestie.", "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."],
-    social: [{
-        name: "mail",
-        display: 'dev.sachin@gmail.com',
-        text: 'mailto:dev.sachin@gmail.com'
-    }, {
-        name: "phone",
-        display: '(+91) 800 600 1000',
-        text: 'tel:+918006001000'
-    }]
-}
+  name: "Sachin Desai",
+  image: "./assets/user.jpg",
+  designation: "Front-end developer",
+  detail: [
+    "Self-motivated developer, who is willing to learn and create outstanding UI applications. Donec aliquam est dui, vel vestibulum diam sollicitudin id.Quisque feugiat malesuada molestie.",
+    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  ],
+  social: [
+    {
+      name: "mail",
+      display: "dev.sachin@gmail.com",
+      text: "mailto:dev.sachin@gmail.com",
+    },
+    {
+      name: "phone",
+      display: "(+91) 800 600 1000",
+      text: "tel:+918006001000",
+    },
+  ],
+};
 export const skills = [
-    {
-        name: 'React',
-        score: 50
-    },
-    {
-        name: 'Javascript',
-        score: 60
-    },
-    {
-        name: 'Css',
-        score: 90
-    },
-    {
-        name: 'Vue',
-        score: 40
-    },
-    {
-        name: 'Redux',
-        score: 40
-    },
-    {
-        name: 'React Native',
-        score: 30
-    },
-]
+  {
+    name: "React",
+    score: 50,
+  },
+  {
+    name: "Javascript",
+    score: 60,
+  },
+  {
+    name: "Css",
+    score: 90,
+  },
+  {
+    name: "Vue",
+    score: 40,
+  },
+  {
+    name: "Redux",
+    score: 40,
+  },
+  {
+    name: "React Native",
+    score: 30,
+  },
+];
 export const hobbies = [
-    {
-        name: "Gaming",
-        image: 'https://picsum.photos/380/140',
-        detail: 'Quisque feugiat malesuada molestie.'
-    },
-    {
-        name: "Cooking",
-        image: 'https://picsum.photos/380/140',
-        detail: 'Quisque feugiat malesuada molestie.'
-    },
-    {
-        name: "Biking",
-        image: 'https://picsum.photos/380/140',
-        detail: 'Quisque feugiat malesuada molestie.'
-    },
-]
+  {
+    name: "Gaming",
+    image: "./assets/gamming.png",
+    detail: "Quisque feugiat malesuada molestie.",
+  },
+  {
+    name: "Cooking",
+    image: "./assets/cooking.png",
+    detail: "Quisque feugiat malesuada molestie.",
+  },
+  {
+    name: "Biking",
+    image: "./assets/biking.png",
+    detail: "Quisque feugiat malesuada molestie.",
+  },
+];
 export const blog = {
-    title: 'How to organize your CSS',
-    image: 'https://picsum.photos/450/450',
-    detail: ["In this article I tell the story about Proin eu justo sit amet lacus bibendum tincidunt. Vivamus non volutpat nisl, a luctus mi.", "Donec aliquam est dui, vel vestibulum diam sollicitudin id.Quisque feugiat malesuada molestie."],
-    link: 'dev.to'
-}
+  title: "How to organize your CSS",
+  image: "./assets/blog.png",
+  detail: [
+    "In this article I tell the story about Proin eu justo sit amet lacus bibendum tincidunt. Vivamus non volutpat nisl, a luctus mi.",
+    "Donec aliquam est dui, vel vestibulum diam sollicitudin id.Quisque feugiat malesuada molestie.",
+  ],
+  link: "dev.to",
+};
 export const experiences = [
-    {
-        image: 'https://picsum.photos/120/80',
-        date: 'Feb 2017 - Current',
-        designation: 'Front-end developer',
-        detail: 'Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.'
-    },
-    {
-        image: 'https://picsum.photos/120/80',
-        date: 'Aug 2016 - Feb 2018',
-        designation: 'Full-stack developer',
-        detail: 'Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.'
-    }
-]
+  {
+    image: "https://picsum.photos/120/80",
+    date: "Feb 2017 - Current",
+    designation: "Front-end developer",
+    detail:
+      "Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.",
+  },
+  {
+    image: "https://picsum.photos/120/80",
+    date: "Aug 2016 - Feb 2018",
+    designation: "Full-stack developer",
+    detail:
+      "Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.",
+  },
+];
 export const projects = [
-    {
-        id: 1,
-        title: 'project 1',
-        image: './assets/react.png',
-        category: 'react',
-        tags: ['HTML', 'CSS', 'responsive'],
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 2,
-        title: 'project 2',
-        image: './assets/angular.png',
-        category: 'angular',
-        tags: ['HTML', 'CSS', 'responsive'],
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 3,
-        title: 'project 3',
-        image: './assets/vue.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'vue',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 4,
-        title: 'project 4',
-        image: './assets/react.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'react',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 5,
-        title: 'project 5',
-        image: './assets/angular.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'angular',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 6,
-        title: 'project 6',
-        image: './assets/vue.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'vue',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 7,
-        title: 'project 7',
-        image: './assets/react.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'react',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 8,
-        title: 'project 8',
-        image: './assets/angular.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'angular',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 9,
-        title: 'project 9',
-        image: './assets/vue.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'vue',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 10,
-        title: 'project 10',
-        image: './assets/react.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'react',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 11,
-        title: 'project 11',
-        image: './assets/react.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'react',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    },
-    {
-        id: 12,
-        title: 'project 12',
-        image: './assets/node.png',
-        tags: ['HTML', 'CSS', 'responsive'],
-        category: 'node',
-        detail: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-        demoLink: 'test',
-        gitLink: 'test',
-    }
-]
-
+  {
+    id: 1,
+    title: "project 1",
+    image: "./assets/react.png",
+    category: "react",
+    tags: ["HTML", "CSS", "responsive"],
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 2,
+    title: "project 2",
+    image: "./assets/angular.png",
+    category: "angular",
+    tags: ["HTML", "CSS", "responsive"],
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 3,
+    title: "project 3",
+    image: "./assets/vue.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "vue",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 4,
+    title: "project 4",
+    image: "./assets/react.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "react",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 5,
+    title: "project 5",
+    image: "./assets/angular.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "angular",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 6,
+    title: "project 6",
+    image: "./assets/vue.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "vue",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 7,
+    title: "project 7",
+    image: "./assets/react.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "react",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 8,
+    title: "project 8",
+    image: "./assets/angular.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "angular",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 9,
+    title: "project 9",
+    image: "./assets/vue.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "vue",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 10,
+    title: "project 10",
+    image: "./assets/react.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "react",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 11,
+    title: "project 11",
+    image: "./assets/react.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "react",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+  {
+    id: 12,
+    title: "project 12",
+    image: "./assets/node.png",
+    tags: ["HTML", "CSS", "responsive"],
+    category: "node",
+    detail:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demoLink: "test",
+    gitLink: "test",
+  },
+];
 
 // export const data = [{
 //     "userInfo": {
